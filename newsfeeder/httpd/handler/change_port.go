d0824ec90a2e66986ac49eacdc62253ab75d46c3
wjordansun <56767177+wjordansun@users.noreply.github.com>
2021-07-14 17:35:41 -0400

    Add files via upload

---
+++ newsfeeder/httpd/handler/change_port.go
@@ -0,0 +1,37 @@
+package handler
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"net"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	ipAddress string = "localhost:8000"
+)
+
+func sendChangePortMessage() {
+	fmt.Println("test")
+	conn, err := net.Dial("tcp", ipAddress)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	b := []byte("changeport")
+
+	_, err = io.Copy(conn, bytes.NewReader(b))
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func ChangePort() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		sendChangePortMessage()
+	}
+}
